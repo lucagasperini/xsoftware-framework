df083927a03d889c612cdf73a1a1757ea845773c
Luca Gasperini <info@xsoftware.it>
2019-01-15 18:43:42 +0100

    Hotfix
    Signed-off-by: Luca Gasperini <info@xsoftware.it>

--- xsoftware-framework.php
+++ xsoftware-framework.php
@@ -30,14 +30,17 @@ class xs_framework
                 wp_enqueue_style('xs_framework_admin_style', plugins_url('style/admin.css', __FILE__));
         }
         
-        static function current_user_role($role)
-        {
-                $user_id = get_current_user_id();
+        static function user_role($role, $user_id = NULL)
+        {      
+                if($user_id == NULL)
+                        $user_id = get_current_user_id();
                 if($user_id < 1)
                         return FALSE;
                 
                 $standard_roles = array(0 => 'subscriber', 1 => 'contributor', 2 => 'author', 3 => 'editor', 4 => 'administrator');
-                $user_roles = get_userdata($user_id)->roles; //FIXME: Can user have more roles?
+                $user_roles = get_userdata($user_id)->roles; 
+                if(count($user_roles) != 1) //FIXME: Can user have more roles?
+                        return FALSE;
                 foreach($standard_roles as $key => $value) {
                         if($role == $value)
                                 $find_need_role = $key;
