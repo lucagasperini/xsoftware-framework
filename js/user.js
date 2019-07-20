f705266b9120b095273b00e2d1b18dcf8af58844
Luca Gasperini <info@xsoftware.it>
2019-07-20 17:15:40 +0200

    AUTOMATIC: Cookie expiration time is 28 days.

--- js/user.js
+++ js/user.js
@@ -1,6 +1,6 @@
 function xs_privacy_accept() {
        var date = new Date();
-        date.setTime(date.getTime()+2*60*60*1000);
+        date.setTime(date.getTime()+28*60*60*1000);
         var str='xs_framework_privacy=accept;expires='+date.toGMTString()+';path=/'
         document.cookie = str;
         location.reload();
